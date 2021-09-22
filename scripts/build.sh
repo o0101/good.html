1012988fadcdb74f1fc9b105210ae8af7b22718b
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-09-22 17:15:11 +0000

    Fixed build errors

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,11 @@
 
 rm -rf dist/ 
 mkdir -p dist/
-rollup -c vv.rollup.config.js
-cat dist/vv.bundle.bang.js src/bang.js > dist/cat.bang.js
+npx rollup -c vv.rollup.config.js
+cat dist/vv.bundle.bang.js src/bang.js > src/cat.bang.js
 npx webpack -c webpack.config.js
 rm dist/vv.bundle.bang.js
-rm dist/cat.bang.js
+rm src/cat.bang.js
 cp dist/bang.js docs/bang.js
 
 
