4b124656504f89a772e72a076e6bd7a5d1123fb5
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-09-25 12:28:58 +0000

    "Improving"

--- 7guis/components/sg-circle-drawer/script.js
+++ 7guis/components/sg-circle-drawer/script.js
@@ -3,6 +3,7 @@ class CircleDrawer extends Base {
 
   AddCircle(clickEvent) {
     if ( clickEvent.target.matches('sg-circle') ) return;
+    console.log('AddCircle');
 
     const {clientX,clientY} = clickEvent;
     const frame = clickEvent.target.closest('.canvas');
@@ -18,10 +19,10 @@ class CircleDrawer extends Base {
       radius: r
     });
     setState('data', state);
-    console.log(History);
   }
 
   OpenSizer(event) {
+    console.log('OpenSizer');
     if ( event.type === 'contextmenu' ) {
       //event.preventDefault();
     }
@@ -32,13 +33,13 @@ class CircleDrawer extends Base {
   }
 
   SizeSelected(inputEvent) {
+    console.log('SizeSelected');
     const state = cloneState('data');
     const circle = state.circleDrawer.circles.find(({key}) => key === state.circleDrawer.selected);
     // we use path[0] rather than target because we attach to sg-sizer-modal not input[type="range"]
     // just so we can keep all methods in circle drawer
     circle.radius = inputEvent.path[0].valueAsNumber;
     setState('data', state);
-    console.log(History);
   }
 
   CloseSizer(event) {
@@ -47,23 +48,30 @@ class CircleDrawer extends Base {
       // if we get a click on the circle, or 
       // if the click was on the dialog tag, only on its background, which is
       // the host sg-sizer-modal overlay
-    if ( event.target.matches('sg-circle.selected') || event.path[0].closest('dialog') ) return;
+    if ( event.target.matches('sg-circle.selected') || event.path[0].closest('dialog,button') ) return;
+
+    const state = cloneState('data');
+
+    if ( ! state.circleDrawer.selected ) return;
+
+    console.log('CloseSizer');
 
     if ( event.type === 'contextmenu' ) {
       //event.preventDefault();
     }
     event.stopPropagation();
 
-    const state = cloneState('data');
-    state.circleDrawer.selected = ''
+    state.circleDrawer.selected = '';
     setState('data', state);
   }
 
   Undo() {
+    console.log('Undo');
     undoState('data');
   }
 
   Redo() {
+    console.log('Redo');
     redoState('data');
   }
 }
