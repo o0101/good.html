139b04d7327ae6ae4c423329c37da3ea90bb5d00
Cris <22254235+crislin2046@users.noreply.github.com>
2021-10-09 05:10:59 +0000

    "Adding cellophane to cpfiles"

--- scripts/cpfiles.sh
+++ scripts/cpfiles.sh
@@ -3,8 +3,10 @@
 cp -r src/* docs/
 cp -r src/* docs/7guis/
 cp -r src/* docs/ctr/
-cp dist/pack.bang.js docs/bang.js
-cp dist/pack.bang.js docs/7guis/bang.js
-cp dist/pack.bang.js docs/ctr/bang.js
+cp -r src/* docs/cellophane/
+cp dist/pack.bang.js docs/bang.js || :
+cp dist/pack.bang.js docs/7guis/bang.js || :
+cp dist/pack.bang.js docs/ctr/bang.js || :
+cp dist/pack.bang.js docs/cellophane/bang.js || :
 
 
