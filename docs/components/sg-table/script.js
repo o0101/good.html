117ecc8094cf10dcd588bf7a8991397cb06b801a
Cris <22254235+crislin2046@users.noreply.github.com>
2021-10-13 15:21:12 +0000

    Cellophane sg-table much better. Consistent on iOS Safari as well as desktop

--- docs/components/sg-table/script.js
+++ docs/components/sg-table/script.js
@@ -17,13 +17,13 @@ class Table extends Base {
   }
 
   SelectColumn(focusEvent) {
-    const [th] = focusEvent.composedPath();
+    const th = Array.from(focusEvent.composedPath()).find(el => el.localName === 'th');
     const colElement = th.closest('table').querySelector(`col[name="${th.getAttribute('name')}"]`);
     colElement.classList.add('selected');
   }
 
   DeselectColumn(focusEvent) {
-    const [th] = focusEvent.composedPath();
+    const th = Array.from(focusEvent.composedPath()).find(el => el.localName === 'th');
     const colElement = th.closest('table').querySelector(`col[name="${th.getAttribute('name')}"]`);
     colElement.classList.remove('selected');
   }
@@ -108,100 +108,111 @@ class Table extends Base {
 
   *Resizer() {
     picking: while(true) {
-      let event = yield;
-      const {target} = event;
-      sizing: if ( target.matches('.sizer') ) {
-        if ( event.type === 'contextmenu' ) {
-          event.preventDefault();
-        }
+      try {
+        let event = yield;
+        const target = event.composedPath()[0];
+        sizing: if ( target.matches('.sizer') ) {
+          if ( event.type === 'contextmenu' ) {
+            event.preventDefault();
+          }
 
-        if ( Table.#DragSizeStart.has(event.type) ) {
-          const {clientX:startX,clientY:startY} = event.type.includes('touch') ? event.touches[0] : event;
-          const header = target.closest('th');
-          const box = header.closest('.box');
+          if ( Table.#DragSizeStart.has(event.type) ) {
+            const {clientX:startX,clientY:startY} = event.type.includes('touch') ? event.touches[0] : event;
+            const frontEl = target.closest('th');
+            const backEl = frontEl.previousElementSibling || (
+              frontEl.closest('tr').previousElementSibling || 
+              frontEl.closest('table').querySelector('thead').lastElementChild
+            )?.firstElementChild;
+            const box = backEl.closest('.box');
+            const boxStyle = getComputedStyle(box);
+            const minWidth = parseFloat(boxStyle.getPropertyValue('min-width'));
+            const minHeight = parseFloat(boxStyle.getPropertyValue('min-height'));
 
-          if ( target.matches('.column') ) {
-            const columnElement = header.closest('table').querySelector(`colgroup col[name="${header.getAttribute('name')}"]`);
-            const previousColumnElement = columnElement.previousElementSibling;
-            if ( ! previousColumnElement ) continue picking;
-            const widthBack = parseFloat(previousColumnElement.width || previousColumnElement.style.width || header.previousElementSibling.getBoundingClientRect().width);
-            const widthFront = parseFloat(columnElement.width || columnElement.style.width)
-            let newX = startX;
+            if ( target.matches('.column') ) {
+              const columnElement = frontEl.closest('table').querySelector(`colgroup col[name="${frontEl.getAttribute('name')}"]`);
+              const previousColumnElement = columnElement.previousElementSibling;
+              if ( ! previousColumnElement ) continue picking;
+              const widthBack = parseFloat(previousColumnElement.width || previousColumnElement.style.width || backEl.getBoundingClientRect().width);
+              const widthFront = parseFloat(columnElement.width || columnElement.style.width)
+              let newX = startX;
 
-            col_size_dragging: while(true) {
-              header.classList.add('dragging');
-              box.style.overflow = 'hidden';
-              event = yield;
-              if ( event.type === 'contextmenu' ) {
-                continue col_size_dragging;
+              col_size_dragging: while(true) {
+                backEl.classList.add('dragging');
+                box.style.overflow = 'hidden';
+                event = yield;
+                if ( event.type === 'contextmenu' ) {
+                  continue col_size_dragging;
+                }
+                if ( Table.#DragSizeStop.has(event.type) ) {
+                  break col_size_dragging;
+                }
+                if ( event.target.matches('.column.sizer') && event.target !== target ) {
+                  //continue newTarget;
+                  //break col_size_dragging;
+                }
+                ({clientX:newX} = event.type.includes('touch') ? event.touches[0] : event);
+                const [back, front] = newWidth();
+                if ( previousColumnElement.matches('.row-header') ) {
+                  box.style.setProperty(`--row-headers-width`, back);
+                } else {
+                  previousColumnElement.width = back;
+                }
+                columnElement.width = front;
               }
-              if ( Table.#DragSizeStop.has(event.type) ) {
-                break col_size_dragging;
-              }
-              if ( event.target.matches('.column.sizer') && event.target !== target ) {
-                //continue newTarget;
-                //break col_size_dragging;
-              }
-              ({clientX:newX} = event.type.includes('touch') ? event.touches[0] : event);
-              const [back, front] = newWidth();
-              if ( previousColumnElement.matches('.row-header') ) {
-                previousColumnElement.closest('.box').style.setProperty(`--row-headers-width`, back);
-              } else {
-                previousColumnElement.width = back;
-              }
-              columnElement.width = front;
-            }
-            box.style.overflow = 'auto';
-            header.classList.remove('dragging');
+              box.style.overflow = 'auto';
+              backEl.classList.remove('dragging');
 
-            function newWidth() {
-              return [
-                `${(widthBack + newX - startX).toFixed(3)}px`,
-                `${(widthFront + startX - newX).toFixed(3)}px`
-              ];
-            }
-          } else if ( target.matches('.row') ) {
-            const rowElement = header.closest('tr')
-            const previousRowElement = rowElement.previousElementSibling || rowElement.closest('table').querySelector('thead').lastElementChild;
-            if ( ! previousRowElement ) continue picking;
-            const heightBack = parseFloat(previousRowElement.height || previousRowElement.style.height || previousRowElement.getBoundingClientRect().height )
-            const heightFront = parseFloat(rowElement.height || rowElement.style.height || rowElement.getBoundingClientRect().height)
-            let newY = startY;
+              function newWidth() {
+                return [
+                  `${Math.max(minWidth, widthBack + newX - startX).toFixed(3)}px`,
+                  `${Math.max(minWidth, widthFront + startX - newX).toFixed(3)}px`
+                ];
+              }
+            } else if ( target.matches('.row') ) {
+              const rowElement = frontEl.closest('tr');
+              const previousRowElement = backEl.closest('tr');
+              if ( ! previousRowElement ) continue picking;
+              const heightBack = parseFloat(previousRowElement.height || previousRowElement.style.height || previousRowElement.getBoundingClientRect().height )
+              const heightFront = parseFloat(rowElement.height || rowElement.style.height || rowElement.getBoundingClientRect().height)
+              let newY = startY;
 
-            row_size_dragging: while(true) {
-              box.style.overflow = 'hidden';
-              header.classList.add('dragging');
-              event = yield;
-              if ( Table.#DragSizeStop.has(event.type) ) {
-                break row_size_dragging;
+              row_size_dragging: while(true) {
+                box.style.overflow = 'hidden';
+                backEl.classList.add('dragging');
+                event = yield;
+                if ( Table.#DragSizeStop.has(event.type) ) {
+                  break row_size_dragging;
+                }
+                if ( event.type === 'contextmenu' ) {
+                  continue row_size_dragging;
+                }
+                if ( event.target.matches('.row.sizer') && event.target !== target ) {
+                  //continue newTarget;
+                  //break row_size_dragging;
+                }
+                ({clientY:newY} = event.type.includes('touch') ? event.touches[0] : event);
+                const [back, front] = newHeight();
+                if ( previousRowElement.matches('.column-header') ) {
+                  previousRowElement.closest('.box').style.setProperty(`--column-headers-height`, back);
+                } else {
+                  previousRowElement.style.height = back;
+                }
+                //rowElement.style.height = front;
               }
-              if ( event.type === 'contextmenu' ) {
-                continue row_size_dragging;
-              }
-              if ( event.target.matches('.row.sizer') && event.target !== target ) {
-                //continue newTarget;
-                //break row_size_dragging;
-              }
-              ({clientY:newY} = event.type.includes('touch') ? event.touches[0] : event);
-              const [back, front] = newHeight();
-              if ( previousRowElement.matches('.column-header') ) {
-                previousRowElement.closest('.box').style.setProperty(`--column-headers-height`, back);
-              } else {
-                previousRowElement.style.height = back;
-              }
-              //rowElement.style.height = front;
-            }
-            box.style.overflow = 'auto';
-            header.classList.remove('dragging');
+              box.style.overflow = 'auto';
+              backEl.classList.remove('dragging');
 
-            function newHeight() {
-              return [
-                `${(heightBack + newY - startY).toFixed(3)}px`,
-                `${(heightFront + startY - newY).toFixed(3)}px`
-              ];
+              function newHeight() {
+                return [
+                  `${Math.max(minHeight, heightBack + newY - startY).toFixed(3)}px`,
+                  `${Math.max(minHeight, heightFront + startY - newY).toFixed(3)}px`
+                ];
+              }
             }
           }
         }
+      } catch(e) {
+        alert(e);
       }
     }
   }
